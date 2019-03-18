80f08a54dd419b3c69331a06fe7418803432756c
Yikun Jiang <yikunkero@gmail.com>
2019-03-18 15:44:19 +0800

    Add ICMP check in security check

--- plugins/provider/securitygroup.py
+++ plugins/provider/securitygroup.py
@@ -29,16 +29,19 @@ class SecurityGroupPlugin(Plugin):
             self.failed = False
             self.reasons = []
             tcp_rule = 'openstack --os-cloud %s security group rule list default \
-            --protocol tcp --ingress -f value \
-            -c "IP Range" -c "Port Range" -c "Ethertype" -c "Remote Security Group" \
-            |grep 0.0.0.0 |grep None | awk \'{print $2}\'' % cloud
+            --ingress -f value \
+            -c "IP Protocol" -c "IP Range" -c "Port Range" -c "Ethertype" -c "Remote Security Group" \
+            |grep 0.0.0.0 |grep None' % cloud
             res = commands.getoutput(tcp_rule)
-            if "19885:19885" not in res:
+            if "tcp 0.0.0.0/0 19885:19885 None" not in res:
                 self.failed = True
                 self.reasons.append("IPV4 ingress 19885 port check failed.")
-            if "22:22" not in res:
+            if "tcp 0.0.0.0/0 22:22 None" not in res:
                 self.failed = True
                 self.reasons.append("IPV4 ingress 22 port check failed.")
+            if "icmp 0.0.0.0/0  None" not in res:
+                self.failed = True
+                self.reasons.append("ICMP check failed.")
             if self.failed:
                 print("Provider (%s) security group check: \033[1;31m FAILED \033[0m") % cloud
                 print("Reason:")
